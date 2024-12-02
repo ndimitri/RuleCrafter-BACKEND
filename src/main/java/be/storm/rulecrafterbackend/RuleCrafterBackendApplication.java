43c0aa67a243e92667c9debf36b46a02c0a84b3d
Nicolas Dimitriadis <dimitriadisnicolas@gmail.com>
2024-12-02 13:36:15 +0100

    git init

---
+++ src/main/java/be/storm/rulecrafterbackend/RuleCrafterBackendApplication.java
@@ -0,0 +1,13 @@
+package be.storm.rulecrafterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleCrafterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleCrafterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/storm/rulecrafterbackend/RuleCrafterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.storm.rulecrafterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleCrafterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
